c77c498e719751b9fbfed84375ef2b3551051f75
Ivo Evans <ievans.storrie@gmail.com>
2020-06-03 23:40:15 +0100

    feat(app): add animated page transitions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Home from "./pages/home";
 
-import Nav from "./Nav";
+const AnimatedSwitch = withRouter(({ location }) => (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="slide" timeout={1000}>
+        <Switch location={location}>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route path="/" component={Home}/>
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  ));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+      <div className="app">
+        <BrowserRouter>
+            <AnimatedSwitch />
+        </BrowserRouter>
+      </div>
   );
 }
 
