d2a332b1dd1aaf76e94ad42adc8f90368995743e
Ivo Evans <ievans.storrie@gmail.com>
2020-06-03 23:18:54 +0100

    feat: add three basic routes and pages, Home, About and Contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import About from './pages/about'
+import Contact from './pages/contact'
+import Home from './pages/home'
 
 function App() {
-  return null;
+  return (
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+  );
 }
 
 export default App;
