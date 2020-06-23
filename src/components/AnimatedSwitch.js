39b69663a6d50dc66600cbee207838d8b4d685d9
Ivo Evans <ievans.storrie@gmail.com>
2020-06-23 14:12:58 +0100

    docs(animatedswitch): add some explanations

--- src/components/AnimatedSwitch.js
+++ src/components/AnimatedSwitch.js
@@ -1,3 +1,12 @@
+/* 
+TOP LEVEL DOC COMMENT
+
+The purpose of this component is to animate transitions between pages.
+During the transition two pages are visible on the screen, but the 
+oncoming page must have opposite styling to the exiting page. For this
+reason a global switch for styling is insufficient, and you need a more 
+involved approach. I used useReducer to mediate class possession for pages.
+*/
 import React from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
@@ -19,7 +28,9 @@ const initialState = {
 export default function AnimatedSwitch() {
   const location = useLocation();
 
+  // this function runs after animations finish to set up correct classes for the next animation
   function updateState(state) {
+    // step 1: switch which page is currenPage
     const newState = { ...state };
     const newRoutes = { ...newState.routes };
     Object.keys(newRoutes).forEach((route) => {
@@ -31,6 +42,7 @@ export default function AnimatedSwitch() {
     });
     newState.routes = newRoutes;
 
+    // step 2: switch which class currentPage refers to
     const intermediary = state.notCurrentPage;
     newState.notCurrentPage = state.currentPage;
     newState.currentPage = intermediary;
