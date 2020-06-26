b0eddd45eac396b8f6aeb60071cfe05d9370192d
Ivo Evans <ievans.storrie@gmail.com>
2020-06-26 10:57:40 +0100

    refactor: lift variables and media query up to body {} in index.css

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -7,10 +7,10 @@ export default function Contact({ classInjection }) {
   return (
     <div className={classInjection}>
       <Nav />
-      <div className="banner">
-        <h2 className="name">Ivo Evans</h2>
+      <div className="contact__banner">
+        <h2 className="contact__name">Ivo Evans</h2>
         <h1>(+44)7922 087726</h1>
-        <h2 className="email">ievans.storrie@gmail.com</h2>
+        <h2 className="contact__email">ievans.storrie@gmail.com</h2>
       </div>
     </div>
   );
