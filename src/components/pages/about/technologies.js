bcf97c623d35428a2c6d89c55fcbd1ea14027c87
Ivo Evans <ievans.storrie@gmail.com>
2020-07-02 16:14:06 +0100

    feat(technologies): create and style Logo component
    Relates #6

--- src/components/pages/about/technologies.js
+++ src/components/pages/about/technologies.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Icon } from "@iconify/react";
-import "./technologies.css";
 
 import html5 from "@iconify/icons-logos/html-5";
 import css3 from "@iconify/icons-logos/css-3";
@@ -30,205 +28,44 @@ import githubOctocat from "@iconify/icons-logos/github-octocat";
 import figmaIcon from "@iconify/icons-logos/figma";
 import jiraIcon from "@iconify/icons-logos/jira";
 
-// comment to self
-// this is interesting but there are things to consider
-// 1. These don't all look so good on dark theme. express and heroku are the worst, but many are bad
-// 2. consider an on-hover or on hover or on focus underline naming the thing which uses the image's alt prop. It could use the same code as the navbar underline with the css attr() function
-// 2.1 while we're at it you should also apply navbar hover effects to focus. Accessibility yo.
-// 3. There's a lot of hardcoding here. Is there a possibility to map over icons instead? Would that still be possible with custom alt tags?
-// 4. This currently takes up a LOT of screen real estate... almost like a whole page's worth of real estate...
-// 5. The h2s should be styled differently here, so that they're less prominent.
-// 6. This could be further abstracted so that each section just takes a title and an array of icons, then makes them for you.
-// 7. Maybe you should work out why Hannah's looks so much better lol. Did she size them in some editor then export them as svgs?
+import Logo from "./logo";
+import "./technologies.css";
 
 export default function Technologies() {
   return (
-    <div>
-      <h1>TECHNOLOGIES</h1>
-      <div className="about__technologies">
-        {/* <section className="about__section">
-        <h2>Languages</h2> */}
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={html5}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={css3}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={javascriptIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={rubyIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={pythonIcon}
-        />
-        {/* </section>
-      <section className="about__section">
-        <h2>Backend</h2> */}
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={postgresqlIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={mongodbIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={nodejsIcon}
-        />
-        <Icon
-          className="about__icon about__express-icon"
-          height="2em"
-          align="center, middle, meet"
-          icon={expressIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={flaskIcon}
-        />
-        {/* </section>
-      <section className="about__section"> 
-        <h2>Frontend</h2> */}
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={reactIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={materialUi}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={styledComponents}
-        />
-        {/* </section>
-
-      <section className="about__section">
-        <h2>Tools</h2> */}
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={jestIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={postmanIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={codecovIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={travisCi}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={prettierIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={eslintIcon}
-        />
-        {/* </section>
-
-      <section className="about__section">
-        <h2>Deployment</h2> */}
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={netlifyIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={herokuIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={googleCloudPlatform}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={dockerIcon}
-        />
-        {/* </section>
-      <section className="about__section">
-        <h2>Project management</h2> */}
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={gitIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={githubOctocat}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={figmaIcon}
-        />
-        <Icon
-          className="about__icon"
-          height="3em"
-          align="center, middle, meet"
-          icon={jiraIcon}
-        />
-        {/* </section> */}
-      </div>
+    <div className="about__technologies">
+      <Logo icon={html5} description="HTML" />
+      <Logo icon={css3} description="CSS" />
+      <Logo icon={javascriptIcon} description="JavaScript" />
+      <Logo icon={rubyIcon} description="Ruby" />
+      <Logo icon={pythonIcon} description="Python" />
+      <Logo icon={postgresqlIcon} description="PostgreSQL" />
+      <Logo icon={mongodbIcon} description="MongoDB" />
+      <Logo icon={nodejsIcon} description="Node.JS" />
+      <Logo
+        extraClasses="about__express-icon"
+        height="2em"
+        icon={expressIcon}
+        description="Express"
+      />
+      <Logo icon={flaskIcon} description="Flask" />
+      <Logo icon={reactIcon} description="React" />
+      <Logo icon={materialUi} description="Material UI" />
+      <Logo icon={styledComponents} description="Styled Components" />
+      <Logo icon={jestIcon} description="Jest" />
+      <Logo icon={postmanIcon} description="Postman" />
+      <Logo icon={codecovIcon} description="CodeCov" />
+      <Logo icon={travisCi} description="Travis CI" />
+      <Logo icon={prettierIcon} description="Prettier" />
+      <Logo icon={eslintIcon} description="ESLint" />
+      <Logo icon={netlifyIcon} description="Netlify" />
+      <Logo icon={herokuIcon} description="Heroku" />
+      <Logo icon={googleCloudPlatform} description="Google Cloud" />
+      <Logo icon={dockerIcon} description="Docker" />
+      <Logo icon={gitIcon} description="Git" />
+      <Logo icon={githubOctocat} description="GitHub" />
+      <Logo icon={figmaIcon} description="Figma" />
+      <Logo icon={jiraIcon} description="Jira" />
     </div>
   );
 }
