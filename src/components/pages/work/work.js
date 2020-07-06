5839ce5c032c31c543ca982768a28ea8b2b73ac1
Ivo Evans <ievans.storrie@gmail.com>
2020-07-06 16:55:37 +0100

    feat(work): add first iteration of projects
    Relates #7

--- src/components/pages/work/work.js
+++ src/components/pages/work/work.js
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../page.css";
+
 import Nav from "../../Nav/Nav";
 import Project from "./Project";
-import "../page.css";
+
+import humanibaseImage from "../../../assets/images/projects/humanibase_cropped.png";
+import apiImage from "../../../assets/images/projects/api_cropped.jpg";
+import cardleyImage from "../../../assets/images/projects/cardley_cropped.png";
 
 export default function Work({ classInjection }) {
   return (
@@ -12,13 +17,25 @@ export default function Work({ classInjection }) {
         name="Humanibase"
         description="Humanibase is a platform for hosting and uploading audio workshops for those in refugee camps. We developed it for Zaatari Radio, which operates a radio station across Jordan and London."
         tech={["React", "Lambda functions", "Airtable"]}
+        picture={humanibaseImage}
+        codeLink="https://github.com/fac19/Zaatari-Radio"
+        deployLink="https://radio-zaatari.netlify.app/"
       />
       <Project
         name="Cardley REST API"
-        description="This is the API for the flashcard/learning app Cardley. It features user login/signup/signout coded by us with JWTs and password hashing, as well as database table relations allowing us to represent users, decks, cards, and decks shared between users that are in different orders for different users. It also features robust console logging with three modes and has good code coverage."
-        tech={["Node", "Express", "PostgreSQL"]}
+        description="This is the API for the flashcard/learning app Cardley. Highlights include bcrypt+JWT authentication, many-many database relations, good test coverage and robust, adjustable console logging."
+        tech={["Node", "Express", "PostgreSQL", "Jest", "Travis CI"]}
+        picture={apiImage}
+        codeLink="https://github.com/fac19/cardley---backend"
       />
-      <Project name="Cardley" description="" />
+      <Project
+        name="Cardley site"
+        description="This React app provides a UI for the Cardley API and lets users browse public decks, edit their own decks, and practice decks. Plus there is confetti."
+        tech={["React", "Jest", "Styled Components", "Material UI"]}
+        picture={cardleyImage}
+        codeLink="https://github.com/fac19/cardley"
+        deployLink="https://cardley.netlify.app/"
+      />
     </div>
   );
 }
