b2019eecddb2970b0841b27d3e3b6de96f554c1d
Ivo Evans <ievans.storrie@gmail.com>
2020-07-14 17:03:30 +0100

    feat(infobox): make component mobile-responsive
    Relates #46

--- src/components/pages/technologies/technologies.js
+++ src/components/pages/technologies/technologies.js
@@ -40,7 +40,6 @@ export default function Technologies({ classInjection }) {
           );
         })}
       </div>
-
       <InfoBox
         name={selectedTech.name}
         icon={selectedTech.icon}
