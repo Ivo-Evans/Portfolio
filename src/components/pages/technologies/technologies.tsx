5c829e8866b12e262c699ff48dd8f2b277f9a24e
Ivo Evans <ievans.storrie@gmail.com>
2020-07-15 10:28:16 +0100

    refactor(*): switch some divs for semantic equivalents

--- src/components/pages/technologies/technologies.tsx
+++ src/components/pages/technologies/technologies.tsx
@@ -6,45 +6,46 @@ import InfoBox from "./children/InfoBox";
 import Nav from "../../Nav/Nav";
 import "./technologies.css";
 import "../../../animations.css";
-import { ClassInjectionType } from "../../classInjectionType"
-import * as types from "./types"
-
+import { ClassInjectionType } from "../../classInjectionType";
+import * as types from "./types";
 
 export default function Technologies({ classInjection }: ClassInjectionType) {
-  const [selectedTech, setSelectedTech] = React.useState<types.ISelectedTech>({});
+  const [selectedTech, setSelectedTech] = React.useState<types.ISelectedTech>(
+    {},
+  );
   return (
     <div className={classInjection}>
       <Nav />
-      <div className="technologies__list">
-        {technologyList.map((technology) => {
-          return (
-            <div
-              role="button"
-              tabIndex={-1}
-              onClick={() => {
-                setSelectedTech({ enabled: true, ...technology });
-              }}
-              onKeyPress={(e) => {
-                if (e.key === "Enter" || e.key === " ") {
+      <main>
+        <article className="technologies__list">
+          {technologyList.map((technology) => {
+            return (
+              <div
+                role="button"
+                tabIndex={-1}
+                onClick={() => {
                   setSelectedTech({ enabled: true, ...technology });
-                }
-              }}
-            >
-              <Logo
-                icon={technology.icon}
-                description={technology.name}
-                extraClasses={`${technology.classModifier} ${
-                  selectedTech.name === technology.name &&
-                  "tech-list__icon--selected"
-                }`}
-                height={technology.heightModifier}
-              />
-            </div>
-          );
-        })}
-      </div>
-      {
-          typeof selectedTech === "object" &&
+                }}
+                onKeyPress={(e) => {
+                  if (e.key === "Enter" || e.key === " ") {
+                    setSelectedTech({ enabled: true, ...technology });
+                  }
+                }}
+              >
+                <Logo
+                  icon={technology.icon}
+                  description={technology.name}
+                  extraClasses={`${technology.classModifier} ${
+                    selectedTech.name === technology.name &&
+                    "tech-list__icon--selected"
+                  }`}
+                  height={technology.heightModifier}
+                />
+              </div>
+            );
+          })}
+        </article>
+        {typeof selectedTech === "object" && (
           <InfoBox
             name={selectedTech.name}
             icon={selectedTech.icon}
@@ -52,7 +53,8 @@ export default function Technologies({ classInjection }: ClassInjectionType) {
             links={selectedTech.links}
             classModifier={selectedTech.classModifier}
           />
-      }
+        )}
+      </main>
     </div>
   );
 }
